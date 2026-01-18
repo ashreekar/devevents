9f50073eacda58954b406c8aa5eac60c2a932673
ashreekar <ashreekar767@gmail.com>
2026-01-18 16:13:17 +0530

    add landing page and button comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import ExploreBtn from "@/components/ExploreBtn";
 
 const Page = () => {
   return (
-    <h1>
-      Welcome to NextJS
-    </h1>
+    <section className="text-center">
+      <h1>The Hub For Every Dev <br /> Events you can't miss</h1>
+      <p className="text-center mt-5">Hackathons, meetups and conferences. All in one place</p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {
+            [1, 2, 3, 4, 5, 6].map((event) => (
+              <li key={event}>Event {event}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </section>
   )
 }
 
