e3a219136448c32f210d58f64297fea30194f0f2
ashreekar <ashreekar767@gmail.com>
2026-01-21 21:52:21 +0530

    add: server action for events recomendation

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import Event from "@/database/event.model";
+import connectDB from "../mongodb";
+
+export const getSimilarEventsSlug = async (slug: string) => {
+    try {
+        await connectDB();
+
+        const event = await Event.findOne({ slug });
+        const similarEvent = await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } });
+
+        return similarEvent;
+    } catch (error) {
+        return [];
+    }
+}
